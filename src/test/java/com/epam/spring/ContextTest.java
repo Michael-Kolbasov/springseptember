fb4ae47b1c18a6558532036fd4264e684792315c
JATragedian <JATragedian@yandex.ru>
2019-09-19 14:07:56 +0300

    Adds homework1.

--- src/test/java/com/epam/spring/ContextTest.java
+++ src/test/java/com/epam/spring/ContextTest.java
@@ -14,19 +14,22 @@ import static org.junit.Assert.assertNotNull;
  * hints: module - spring test, test runner classes
  */
 public class ContextTest {
-
-    TestDinosaur dino;
+    
     ClassPathXmlApplicationContext context;
     
     @Before
     public void init() {
         context = new ClassPathXmlApplicationContext("testContext.xml");
-        dino = (TestDinosaur) context.getBean("dinosaur");
     }
 
     @Test
     public void testContextLoads() {
         assertNotNull(context);
+    }
+    
+    @Test
+    public void testFactoryWithContext() {
+        TestDinosaur dino = (TestDinosaur) context.getBean("dinosaur");
         assertNotNull(dino);
         dino.doRawr();
     }
