9d2506eb104c4382ae14f8611cf23d4c5abe263e
Maksim <Maksim_Golota@epam.com>
2019-09-23 18:12:10 +0300

    done homework #1

--- src/test/java/com/epam/spring/ContextTest.java
+++ src/test/java/com/epam/spring/ContextTest.java
@@ -1,21 +1,48 @@
 package com.epam.spring;
 
+import com.epam.spring.model.Cat;
+import com.epam.spring.services.CatFightingService;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.Assert.assertNotNull;
 
 /**
  * hw1
  * Load test application context
- *
+ * <p>
  * hints: module - spring test, test runner classes
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:testContext.xml"})
 public class ContextTest {
 
+    @Autowired
+    CatFightingService cfs;
+
+    @MockBean(name = "oldCat")
+    private Cat oldCat;
+
+    @MockBean(name = "youngCat")
+    private Cat youngCat;
+
+    @Before
+    public void initTest() {
+        Mockito.when(oldCat.getAge()).thenReturn(10);
+        Mockito.when(youngCat.getAge()).thenReturn(5);
+        Mockito.when(oldCat.getName()).thenReturn("Older");
+        Mockito.when(youngCat.getName()).thenReturn("Younger");
+    }
+
     @Test
-    public void testContextLoads() {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("testContext.xml");
-        assertNotNull(context);
+    public void whoIsOlderTest() {
+        Cat cat = cfs.whoIsOlder(oldCat, youngCat);
+        Assert.assertEquals("older", cat.getName().toLowerCase());
     }
 }
