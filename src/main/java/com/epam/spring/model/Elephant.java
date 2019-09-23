083a85463b1ee82dcd43ffc0aa332601a3831c8f
Nikolai Tiutiunnik <puffbeatsale@gmail.com>
2019-09-23 22:45:04 +0300

    javadocs added

--- src/main/java/com/epam/spring/model/Elephant.java
+++ src/main/java/com/epam/spring/model/Elephant.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Elephant class with fields and methods set in context.
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
--- src/test/java/com/epam/spring/ElephantTest.java
+++ src/test/java/com/epam/spring/ElephantTest.java
@@ -9,6 +9,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.junit.Assert.assertEquals;
 
+/**
+ * ElephantTest checks if the elephant bean was loaded from ApplicationContext and his name matches context-set name.
+ * We need to specify the location of context in order to get rid of context mistakes.
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:ElephantTest-context.xml"})
 public class ElephantTest {
