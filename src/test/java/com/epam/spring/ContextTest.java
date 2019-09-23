eb7ad2d8cd1478a01bc22a0a0f47fb1d9e07e0cc
Tukes <xtukesx@gmail.com>
2019-09-23 03:16:47 +0300

    Implemented test that verifies that there is a bean with configuration in the context

--- src/test/java/com/epam/spring/ContextTest.java
+++ src/test/java/com/epam/spring/ContextTest.java
@@ -1,7 +1,12 @@
 package com.epam.spring;
 
+import com.epam.spring.config.TestConfig;
 import org.junit.Test;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertNotNull;
 
@@ -11,11 +16,15 @@ import static org.junit.Assert.assertNotNull;
  *
  * hints: module - spring test, test runner classes
  */
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = TestConfig.class)
 public class ContextTest {
 
-    @Test
-    public void testContextLoads() {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("testContext.xml");
-        assertNotNull(context);
-    }
+  @Autowired
+  private ApplicationContext ctx;
+
+  @Test
+  public void testContextLoads() {
+    assertNotNull("There is no bean with testConfig name", ctx.getBean(TestConfig.class));
+  }
 }
